7081aa0aa8e449cd8ed719b44dcc776509f0fbff
Jinhyeok Lee <zenyr@zenyr.com>
2022-09-25 10:57:04 +0000

    Update 7 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import { Button, Text } from '@mantine/core';
 import React, { useState } from 'react';
-import { MantineProvider, Text } from '@mantine/core';
-import { Global } from '@mantine/core';
+import { AppShell, Navbar, Header } from '@mantine/core';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <MantineProvider>
-      <Global />
-      <div className="App">
-        <Text>Welcome to Mantine!</Text>
-      </div>
-    </MantineProvider>
+    <AppShell
+      padding="md"
+      navbar={<Navbar width={{ base: 300 }} height={500} p="xs">{/* Navbar content */}</Navbar>}
+      header={<Header height={60} p="xs">{/* Header content */}</Header>}
+      styles={(theme) => ({
+        main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+      })}
+    >
+      <Text>Welcome to Mantine! {count}</Text>
+      <Button onClick={() => setCount((v) => v + 1)}>Add</Button>
+    </AppShell>
+
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { MantineProvider, Text } from '@mantine/core';
+import { Global, Button } from '@mantine/core';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      withGlobalStyles withNormalizeCSS
+      theme={{
+        fontFamily: 'Pretendard-Regular, sans-serif',
+      }}
+    >
+      <Global styles={[
+        {
+          '@font-face': {
+            fontFamily: 'Pretendard-Regular',
+            src: "url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff')",
+            fontWeight: 400,
+            fontStyle: 'normal',
+          },
+          'html,body,#root': {
+            height: '100%'
+          }
+        }
+      ]} />
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
